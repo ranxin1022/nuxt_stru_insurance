ec910a4dafd77a13f3fe896f8e2a1c7b8b56077c
ranxin <rachel.ran@jfz.com>
2019-01-24 20:10:49 +0800

    initial

---
+++ pages/test.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <el-date-picker v-model="formTwoDate"
+                      type="date"
+                      :clearable="false"
+                      :editable="false"
+                      value-format="yyyy-MM-dd"
+                      :picker-options="pickerOptions">
+      </el-date-picker>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "test",
+
+        data() {
+          return {
+            formTwoDate: '',
+          }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
